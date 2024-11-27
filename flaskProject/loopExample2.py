93f27d0f92f893e92e14d8ca2f34a6f473b8e270
maniluisa <saminsuas@gmail.com>
2024-11-26 20:45:36 -0500

    loopExample3

---
+++ flaskProject/loopExample2.py
@@ -0,0 +1,5 @@
+i = 1
+while i <= 50:
+    print(i)
+    i = 3 * i + 1
+print('Programa terminado')
---
+++ flaskProject/loopExample3.py
@@ -0,0 +1,5 @@
+número = int(input('Escriba un número positivo: '))
+while número < 0:
+    print('¡Ha escrito un número negativo! Inténtelo de nuevo')
+    número = int(input('Escriba un número positivo: '))
+print('Gracias por su colaboración')
