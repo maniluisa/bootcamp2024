f330ba8b8bde75c60ce03a4115810504517a6dea
maniluisa <saminsuas@gmail.com>
2024-11-26 21:01:13 -0500

    loop

---
+++ flaskProject/loopActivity1.py
@@ -0,0 +1,5 @@
+answer = input('Continua con un si')
+while answer == 'SI':
+    print('estás dentro del sistema')
+    answer = input('Deseas continuar?')
+print('Has salido correctamente del programa')
